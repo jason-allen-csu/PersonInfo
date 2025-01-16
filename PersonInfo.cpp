7c95b8495b24809ce30f2044b6ec968ddd9a81fa
jason-allen-csu <jason.allen@csuglobal.edu>
2025-01-15 21:31:52 -0500

    Commit 1
    Program for displaying fictional person information

---
+++ PersonInfo.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main(){
+
+	string FirstName = "Albert";
+	string LastName = "Newton";
+	string StreetAddress = "389 Emc Street";
+	string City = "Rome";
+	string ZipCode = "02765";
+
+	cout << FirstName << endl;
+	cout << LastName << endl;
+	cout << StreetAddress << endl;
+	cout << City << endl;
+	cout << ZipCode << endl;
+
+	return 0;
+}
